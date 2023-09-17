e20c785879ced9fbcd8f825bb864134765453b4a
Carlos Santillán <carlosalberto.santillan@mail.polimi.it>
2023-09-17 17:45:31 +0200

    testing step 1 realizations

--- OLA/Step1Experiment.py
+++ OLA/Step1Experiment.py
@@ -1,40 +1,21 @@
 import numpy as np
 
 from OLA.environments import SingleClassEnvironment
-
-"""
-True expected number of clicks curve given bid
-"""
-
-
-def daily_clicks_curve_step1(bid: float):
-    return -3 * (bid - 6) ** 2 + 30
-
-
-def click_curve_noise_step1():
-    return np.random.normal(2, 4)
+from OLA.learners import Step1UCBLearner
+from OLA.learners import Step1TSLearner
+from OLA.environment_properties import click_curve_noise
+from OLA.environment_properties import daily_clicks_curve
+from OLA.environment_properties import click_cumulative_cost_noise
+from OLA.environment_properties import click_cumulative_cost
+from OLA.simulators import simulate_single_class
 
 
-def click_conversion_rate(bid: float):
-    return (-3 * ((bid - 6) ** 2) + 30) / 35
-
-
-def click_cumulative_cost_step1(bid: float):
-    return -bid * (bid - 14) * 50
-
-
-def click_cumulative_noise_step1():
-    return np.random.normal(10, 4)
-
-
-if __name__ == '__main__':
-    N = daily_clicks_curve_step1
-    en = click_curve_noise_step1
-    C = click_cumulative_cost_step1
-    ec = click_curve_noise_step1
+def env_init_step1():
+    N = np.vectorize(daily_clicks_curve)
+    en = click_curve_noise
+    C = np.vectorize(click_cumulative_cost)
+    ec = click_cumulative_cost_noise
     rng = np.random
-    # For completeness' sake, the conversion rate curve
-    # is (-3((x - 6) ** 2) + 30) / 35
     A = {
         400: 0.51,
         500: 0.77,
@@ -43,4 +24,14 @@ if __name__ == '__main__':
         800: 0.51
     }
 
-    env = SingleClassEnvironment(N, en, C, ec, A, rng)
+    return SingleClassEnvironment(N, en, C, ec, A, rng)
+
+
+def ucb1_learner_init(env: SingleClassEnvironment):
+    bids = np.array(4, 5, 6, 7, 8, 9)
+    price = np.array(400, 500, 600, 700, 800)
+    return Step1UCBLearner(env, bids, price)
+
+
+if __name__ == '__main__':
+    simulate_single_class(env_init_step1, ucb1_learner_init, 365)
