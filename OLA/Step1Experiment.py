1e705635a02f2dd49f8e144a8d7a8e8a46862bad
Carlos Santillán <carlosalberto.santillan@mail.polimi.it>
2023-09-17 20:08:42 +0200

    step 1 experiment

--- OLA/Step1Experiment.py
+++ OLA/Step1Experiment.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 from OLA.environments import SingleClassEnvironment
-from OLA.learners import Step1UCBLearner
-from OLA.learners import Step1TSLearner
+from OLA.base_learners import Step1UCBLearner
+from OLA.base_learners import Step1TSLearner
 from OLA.environment_properties import click_curve_noise
 from OLA.environment_properties import daily_clicks_curve
 from OLA.environment_properties import click_cumulative_cost_noise
 from OLA.environment_properties import click_cumulative_cost
 from OLA.simulators import simulate_single_class
+from OLA.simulators import plot_single_class_sim_result
 
 
 def env_init_step1():
@@ -17,21 +18,22 @@ def env_init_step1():
     ec = click_cumulative_cost_noise
     rng = np.random
     A = {
-        400: 0.51,
-        500: 0.77,
-        600: 0.85,
-        700: 0.77,
-        800: 0.51
+        4: 0.51,
+        5: 0.77,
+        6: 0.85,
+        7: 0.77,
+        8: 0.51,
     }
 
     return SingleClassEnvironment(N, en, C, ec, A, rng)
 
 
 def ucb1_learner_init(env: SingleClassEnvironment):
-    bids = np.array(4, 5, 6, 7, 8, 9)
-    price = np.array(400, 500, 600, 700, 800)
+    bids = np.array([4, 5, 6, 7, 8])
+    price = np.array([4, 5, 6, 7, 8])
     return Step1UCBLearner(env, bids, price)
 
 
 if __name__ == '__main__':
-    simulate_single_class(env_init_step1, ucb1_learner_init, 365)
+    sim_object = simulate_single_class(env_init_step1, ucb1_learner_init, 365)
+    plot_single_class_sim_result(sim_object)
