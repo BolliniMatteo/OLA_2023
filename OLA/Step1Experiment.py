d8a55a61e014d7d9ed5b308e77785bea56f63313
Carlos Santillán <carlosalberto.santillan@mail.polimi.it>
2023-09-18 21:53:54 +0200

    finished steps 1 and 2

--- OLA/Step1Experiment.py
+++ OLA/Step1Experiment.py
@@ -22,13 +22,20 @@ def ucb1_learner_init(env: SingleClassEnvironment, bids: np.ndarray, prices: np.
     return Step1UCBLearner(env, bids, prices)
 
 
+def ts_learner_init(env: SingleClassEnvironment, bids: np.ndarray, prices: np.ndarray, rng):
+    return Step1TSLearner(env, bids, prices, rng)
+
+
 if __name__ == '__main__':
     seed = 1000
     bids = ep.get_bids()
     prices = ep.get_prices()
     rng = np.random.default_rng(seed=seed)
     env_init = lambda: env_init_step1(prices, rng)
-    learner_init = lambda env: ucb1_learner_init(env, bids, prices)
+    learner_init_ucb1 = lambda env: ucb1_learner_init(env, bids, prices)
+    learner_init_ts = lambda env: ts_learner_init(env, bids, prices, rng)
     T = 365
-    sim_object = simulate_single_class(env_init, learner_init, T)
-    plot_single_class_sim_result(sim_object)
+    sim_object_ucb1 = simulate_single_class(env_init, learner_init_ucb1, T)
+    sim_object_ts = simulate_single_class(env_init, learner_init_ts, T)
+    plot_single_class_sim_result(sim_object_ucb1)
+    plot_single_class_sim_result(sim_object_ts)
---
+++ OLA/Step2Experiment.py
@@ -0,0 +1,53 @@
+import numpy as np
+import sklearn.gaussian_process.kernels
+
+from OLA.environments import SingleClassEnvironment
+from OLA.base_learners import Step2UCBLearner
+from OLA.base_learners import Step2TSLearner
+import environment_properties as ep
+from OLA.simulators import simulate_single_class
+from OLA.simulators import plot_single_class_sim_result
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel
+
+
+def env_init_step2(prices: np.ndarray, rng: np.random.Generator):
+    N = ep.daily_clicks_curve
+    en = lambda: ep.click_curve_noise(rng, 1)
+    C = ep.click_cumulative_cost
+    ec = lambda: ep.click_cumulative_cost_noise(rng, 1)
+    A = {p: ep.click_conversion_rate(p) for p in prices}
+
+    return SingleClassEnvironment(N, en, C, ec, A, rng)
+
+
+def gpucb_learner_init(env: SingleClassEnvironment, bids: np.ndarray, prices: np.ndarray,
+                        kernel: sklearn.gaussian_process.kernels.Kernel, alpha: float, beta: float):
+    return Step2UCBLearner(env, bids, prices, kernel, alpha, beta)
+
+
+def gpts_learner_init(env: SingleClassEnvironment, bids: np.ndarray, prices: np.ndarray,
+                      kernel: sklearn.gaussian_process.kernels.Kernel, alpha: float, rng: np.random.Generator):
+    return Step2TSLearner(env, bids, prices, kernel, alpha, rng)
+
+
+if __name__ == '__main__':
+    seed = 1000
+    bids = ep.get_bids()
+    prices = ep.get_prices()
+    rng = np.random.default_rng(seed=seed)
+    env_init = lambda: env_init_step2(prices, rng)
+
+    kernel = ConstantKernel(1.0, (1e-3, 1e3)) * RBF(1.0, (1e-3, 1e3))
+    alpha = 10
+    # beta should be around 110
+    beta = 110
+
+    learner_init_gpucb = lambda env: gpucb_learner_init(env, bids, prices, kernel, alpha, beta)
+    learner_init_gpts = lambda env: gpts_learner_init(env, bids, prices, kernel, alpha, rng)
+
+    T = 365
+    #sim_object_gpucb = simulate_single_class(env_init, learner_init_gpucb, T, n_runs=20)
+    sim_object_ts = simulate_single_class(env_init, learner_init_gpts, T, n_runs=20)
+    #plot_single_class_sim_result(sim_object_gpucb)
+    plot_single_class_sim_result(sim_object_ts)
