4bc265411bb5c61b88320c9bce35efe66ffa80e8
Carlos Santillán <carlosalberto.santillan@mail.polimi.it>
2023-12-13 15:30:02 +0100

    finished ucb1-cd

--- OLA/base_learners.py
+++ OLA/base_learners.py
@@ -225,10 +225,11 @@ class Step5UCBLearner(SingleClassLearnerNonStationary):
 class Step5UCBChangeDetectorLearner(SingleClassLearnerNonStationary):
     def __init__(self, environment: envs.SingleClassEnvironmentNonStationary,
                  bids: np.ndarray, prices: np.ndarray, c: float, burn_in_steps: int,
-                 epsilon=0.05, max_num_changes=2,
+                 epsilon=0.05, max_num_changes=5,
                  time_horizon=365):
         super().__init__(environment, bids, prices)
-        self.h = np.log(time_horizon / max_num_changes)
+        # self.h = np.log(time_horizon / max_num_changes)
+        self.h = 0.3
         self.c = c
         self.epsilon = epsilon
         self.cur_rand_walk_upper = 0
@@ -246,15 +247,18 @@ class Step5UCBChangeDetectorLearner(SingleClassLearnerNonStationary):
         c_est = self.env.C(self.xs)
         # we first wait for the burn-in-phase to finish (we have a new burn-in every time there's a change)
         if self.rounds_since_last_detection < self.burn_in_steps:
-            if self.rounds_since_last_detection >= self.ps.shape[0]:
+            if self.rounds_since_last_detection < self.ps.shape[0]:
                 p_t = self.ps[self.rounds_since_last_detection]
+                p_t_ind = self.rounds_since_last_detection
                 alpha = 0.5
                 x_t, x_t_ind = single_class_bid_opt(self.xs, p_t, alpha, n_est, c_est, self.env.prod_cost)
             else:
+                # play normally
                 alphas_est = self.estimator.provide_estimations(lower_bound=False)
                 x_t, x_t_ind, p_t, p_t_ind = single_class_opt(self.xs, self.ps, alphas_est, n_est, c_est,
                                                               self.env.prod_cost)
-            self.play_and_save(x_t, p_t)
+            n, q, c = self.play_and_save(x_t, p_t)
+            self.estimator.update_estimations(p_t_ind, q, n)
             # If we end the burn-in then we set everything up for the next round,
             # where the actual change detection starts
             if self.rounds_since_last_detection == (self.burn_in_steps - 1):
@@ -264,30 +268,53 @@ class Step5UCBChangeDetectorLearner(SingleClassLearnerNonStationary):
                 self.mu_0 = np.mean(estimates)
                 step_upper = estimates[-1] - self.mu_0 - self.epsilon
                 step_lower = self.mu_0 - estimates[-1] - self.epsilon
-                self.cur_rand_walk_lower = np.max(0, self.cur_rand_walk_lower + step_upper)
-                self.cur_rand_walk_lower = np.max(0, self.cur_rand_walk_upper + step_lower)
+                # print('here we are')
+                # print('self.mu0 = {}'.format(self.mu_0))
+                # print('step_upper = {}'.format(step_upper))
+                # print('step_lower = {}'.format(step_lower))
+                self.cur_rand_walk_lower = np.max([0, self.cur_rand_walk_lower + step_lower])
+                self.cur_rand_walk_upper = np.max([0, self.cur_rand_walk_upper + step_upper])
 
         else:
             # play normally
             alphas_est = self.estimator.provide_estimations(lower_bound=False)
             x_t, x_t_ind, p_t, p_t_ind = single_class_opt(self.xs, self.ps, alphas_est, n_est, c_est,
                                                           self.env.prod_cost)
-            self.play_and_save(x_t, p_t)
+            n, q, c = self.play_and_save(x_t, p_t)
+            self.estimator.update_estimations(p_t_ind, q, n)
             # calculate steps
             last_reward = self.history.bernoulli_estimates()[-1]
             step_upper = last_reward - self.mu_0 - self.epsilon
             step_lower = self.mu_0 - last_reward - self.epsilon
             # update bounds
-            self.cur_rand_walk_lower = np.max(0, self.cur_rand_walk_lower + step_upper)
-            self.cur_rand_walk_lower = np.max(0, self.cur_rand_walk_upper + step_lower)
+            self.cur_rand_walk_lower = np.max([0, self.cur_rand_walk_lower + step_lower])
+            self.cur_rand_walk_upper = np.max([0, self.cur_rand_walk_upper + step_upper])
             # see if we identify a change after update (could be a false alarm...)
+            # print('lower bound walk = {}'.format(self.cur_rand_walk_lower))
+            # print('upper ')
             if self.cur_rand_walk_lower > self.h or self.cur_rand_walk_upper > self.h:
-                # change detectedS
+                # change detected
+                # print('CHANGE DETECTED')
                 self.rounds_since_last_detection = -1
                 # clear previous estimates
                 self.estimator.reset_estimates()
         self.rounds_since_last_detection += 1
 
+
+class Step6EXP3Learner(SingleClassLearnerNonStationary):
+    def __init__(self, environment: envs.SingleClassEnvironmentNonStationary, bids: np.ndarray, prices: np.ndarray):
+        super().__init__(environment, bids, prices)
+        self.estimator = est.BeExp3Estimator(prices)
+
+    def play_round(self):
+        if self.history.played_rounds() < self.ps.shape[0]:
+            p_t = self.prices[self.history.played_rounds()]
+            p_t_ind = self.history.played_rounds()
+        else:
+            p_t, p_t_ind = self.estimator.provide_arm()
+
+        n, q, c = self.play_and_save(p_t)
+        self.estimator.update_estimations(p_t_ind, q / n)
 
 class Step5UCBWINLearner(SingleClassLearnerNonStationary):
     def __init__(self, environment: envs.SingleClassEnvironmentNonStationary,
@@ -296,32 +323,23 @@ class Step5UCBWINLearner(SingleClassLearnerNonStationary):
         super().__init__(environment, bids, prices)
         self.win_size = win_size
         self.c = c
+        self.estimator = est.BeUCB1SWEstimator(self.ps.shape[0], self.win_size, self.c)
 
     def play_round(self):
         n_est = self.env.N(self.xs)
         c_est = self.env.C(self.xs)
-        t = self.history.played_rounds()
         # first round is t=0
-        estimator = est.BeUCB1Estimator(self.ps.shape[0], self.c)
         # remember that t is excluded in the range()
-        for i in range(max(0, t - self.win_size), t):
-            p_ind = np.where(self.ps == self.history.ps[i])[0][0]
-            estimator.update_estimations(p_ind, self.history.qs[i], self.history.ns[i])
-        non_pulled_arms = estimator.get_non_pulled_arms()
+        non_pulled_arms = self.estimator.get_non_pulled_arms()
         if non_pulled_arms.shape[0] != 0:
             p_t_ind = non_pulled_arms[0]
             p_t = self.ps[p_t_ind]
-            # we have no data in our window
-            # but if we don't find the arm in our window, then it wasn't a good arm in the last win_size days
-            # we can assume that it has a not so large alpha value whn choosing the bid
-            # this collects fewer samples for it, but avoid losing much reward
-            # (with a large estimated alpha we tend to a larger bid, but we can get a negative reward
-            # if we have many clicks and relative cost to pay, but few conversions to profit)
-            x_t, _ = single_class_bid_opt(self.xs, p_t, 0.4, n_est, c_est, self.env.prod_cost)
+            x_t, _ = single_class_bid_opt(self.xs, p_t, 0.5, n_est, c_est, self.env.prod_cost)
         else:
-            alphas_est = estimator.provide_estimations(lower_bound=False)
-            x_t, _, p_t, _ = single_class_opt(self.xs, self.ps, alphas_est, n_est, c_est, self.env.prod_cost)
-        self.play_and_save(x_t, p_t)
+            alphas_est = self.estimator.provide_estimations()
+            x_t, _, p_t, p_t_ind = single_class_opt(self.xs, self.ps, alphas_est, n_est, c_est, self.env.prod_cost)
+        n, q, c = self.play_and_save(x_t, p_t)
+        self.estimator.update_estimations(p_t_ind, q, n)
 
 
 class MultiClassLearner(ABC):
@@ -636,4 +654,3 @@ class Step4UCBOneClassLearner(Step4UCBFixedClassesLearner):
                  kernel: sklearn.gaussian_process.kernels.Kernel, alpha: float, beta: float, ucb_constant: float = 1):
         super().__init__(environment, bids, prices, kernel, alpha, beta, {p: 0 for p in environment.user_profiles},
                          ucb_constant)
-
